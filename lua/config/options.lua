2661250724975483d293c8b58e9578ed05dcdcab
keith-moreno <108557464+k1tooy@users.noreply.github.com>
2024-04-30 06:55:24 +0800

    rosepine theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 vim.o.incsearch = true
 vim.o.hlsearch = false
+vim.o.guicursor = vim.o.guicursor .. ",a:blinkon0"
+vim.o.scrolloff = 5
+vim.o.sidescrolloff = 5
+vim.o.termguicolors = true
